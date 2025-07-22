5dfa9b444f97e76c23c6b0982032d769256eb625
emilekosha <emilezolagarvane@gmail.com>
2025-07-22 13:22:19 +0000

    fix: remove base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/le-perroquet-articles/', // 👈 important pour GitHub Pages
+ base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
+    ? '/le-perroquet-articles/'
+    : '/',
   plugins: [react()],
 });
